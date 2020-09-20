876c3238e1b3ecde9d1d0bb208ceda5a99aaa06e
Gage1218 <47937668+Gage1218@users.noreply.github.com>
2020-09-20 10:19:32 +0800

    Create mian.c

---
+++ mian.c
@@ -0,0 +1,9 @@
+char **letterCombination(char* digtis,int* returnSize){
+    char letter2[3] ={'a','b','c'};
+    char letter3[3] = {'d','e','f'};
+
+    int digtis_len = 0;
+    digtis_len = strlen(digtis); 
+    
+   
+}
